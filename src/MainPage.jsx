d74e246ce56c244074533b9afe5937ff93d96d7c
Debargha Nandi <debarghanandi.india@gmail.com>
2024-10-13 21:21:36 +0530

    update 4

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -8,26 +8,23 @@ const MainPage = () => {
       <h1>Search Categories</h1>
       <div className="cards-container">
         <div className="card">
-          <h3>Search Category 1</h3>
-          <p>Explore and search content for Category 1.</p>
+          <h3>Search Genetics Contents</h3>
           <Link to="/search/category1">
-            <button>Go to Category 1</button>
+            <button>Search</button>
           </Link>
         </div>
 
         <div className="card">
-          <h3>Search Category 2</h3>
-          <p>Explore and search content for Category 2.</p>
+          <h3>Search Plant Breeding Contents</h3>
           <Link to="/search/category2">
-            <button>Go to Category 2</button>
+            <button>Search</button>
           </Link>
         </div>
 
         <div className="card">
-          <h3>Search Category 3</h3>
-          <p>Explore and search content for Category 3.</p>
+          <h3>Search Cytogenetics Contents</h3>
           <Link to="/search/category3">
-            <button>Go to Category 3</button>
+            <button>Search</button>
           </Link>
         </div>
       </div>
